dadeb79a1c295e49fda1f7df143f2c41727b21b8
firacil <firaolmekbib511@gmail.com>
2023-02-16 05:24:51 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-/* main - prints with proper grammar, 
+/*
+ * main - prints with proper grammar,
  * but the outcome is a piece of art,
- * return : Always 0.
+ * Return: Always 0.
  */
 
 
