d3b80c4c83d6213ddf5c1ff1b4fc07e6d638ed33
firacil <firaolmekbib511@gmail.com>
2023-02-16 08:31:15 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return:1
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a newline, to standard error.
+ * Return: Always 1.
  */
 int main(void)
 {
-char ch[] = fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-write(2,  ch, sizeof(ch));
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+  write(2, "and that piece of art is useful\" -Dora kopar, 2015-10-19\n", 59);
+  return (1);
 }
