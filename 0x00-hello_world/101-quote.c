0aa7a20c67957ad4cca8150192dbcede715bfe2d
firacil <firaolmekbib511@gmail.com>
2023-02-16 08:14:09 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return:1
+ */
+int main(void)
+{
+char ch[] = fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2,  ch, sizeof(ch));
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
