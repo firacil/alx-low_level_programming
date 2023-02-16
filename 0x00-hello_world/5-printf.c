d6a31529ff04cbe38bc1086f106900b774e3607e
firacil <firaolmekbib511@gmail.com>
2023-02-16 05:43:01 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+
 /*
  * main - Prints "with proper grammar,but the outcome is
- * a piece of art," followed by a new line.
+ * a piece of art," followed by a new line
  * Return: Always 0.
  */
 
