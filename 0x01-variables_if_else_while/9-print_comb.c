e404c7a0cfcc1e4932c6d65ea97d2d7b107e508a
firacil <firaolmekbib511@gmail.com>
2023-02-17 11:00:21 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,17 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
 		putchar('0' + a);
+
+	if (a < 9)
+	{
 		putchar(',');
 		putchar(' ');
 
 	}
-
+	}
 	putchar('\n');
 	return (0);
 }
