00cbc167c92937c2725a8ecfe89e0f91d8b8a85e
firacil <firaolmekbib511@gmail.com>
2023-02-17 09:47:03 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints a base 10 digits followed by new line.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 10; c++)
+	{
+		printf("%d\n", c);
+	}
+	return (0);
+}
