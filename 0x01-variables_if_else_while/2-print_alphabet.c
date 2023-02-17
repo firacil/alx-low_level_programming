7223a7d50bb784ad2c5878c8ad66ce50ce0c0bae
firacil <firaolmekbib511@gmail.com>
2023-02-17 05:50:10 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower case alphabets.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		return (0);
+
+}
