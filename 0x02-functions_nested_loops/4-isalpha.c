7a6d7ad12f1dbc7cecf044a4b790bb2f13f45825
firacil <firaolmekbib511@gmail.com>
2023-02-20 23:00:55 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,37 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _isalpha - checks alphabetic charachters.
+ * @c: test for charachters.
+ * Return: 0 or 1.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * main - prints calling function.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	putchar(r + '0');
+	r = _isalpha('o');
+	putchar(r + '0');
+	r = _isalpha(108);
+	putchar(r + '0');
+	r = _isalpha(';');
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
+}
