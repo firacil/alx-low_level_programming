aeb5dd8bc7816e71f877c9c4d99f7387d5fe8aaa
firacil <firaolmekbib511@gmail.com>
2023-02-21 02:39:59 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -14,24 +14,3 @@ int _isalpha(int c)
 	else
 		return (0);
 }
-
-/**
- * main - prints calling function.
- * Return: 0.
- */
-
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	putchar(r + '0');
-	r = _isalpha('o');
-	putchar(r + '0');
-	r = _isalpha(108);
-	putchar(r + '0');
-	r = _isalpha(';');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
