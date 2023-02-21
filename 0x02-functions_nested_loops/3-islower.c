fef9e19e8c8c3e2269dbc304ad66953672b5ea5a
firacil <firaolmekbib511@gmail.com>
2023-02-21 02:36:38 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -14,22 +14,3 @@ int _islower(int c)
 	else
 	return (0);
 }
-
-/**
- * main - prints from calling function.
- * Return: 0.
- */
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
