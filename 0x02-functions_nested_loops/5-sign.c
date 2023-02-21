e94c427ce2d28e196a369b76443b1aba1439464b
firacil <firaolmekbib511@gmail.com>
2023-02-21 03:04:00 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,25 @@
 
 /**
  * print_sign - prints the sign of the numbers.
+ * @n: tested.
  * Return: 0.
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
-	{	
+	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		return(-1);
+		return (-1);
 	}
-	else 
+	else
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
 }
