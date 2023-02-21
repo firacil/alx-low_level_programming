52eb9d0c36060ab248ea04da990227809108a31f
firacil <firaolmekbib511@gmail.com>
2023-02-21 02:51:28 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of the numbers.
+ * Return: 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{	
+		_putchar(43);
+		return(1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return(-1);
+	}
+	else 
+	{
+		_putchar(48);
+		return(0);
+	}
+}
