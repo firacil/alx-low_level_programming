bee86d50059d4eb2d1447b0da4cd35e24236d2b6
firacil <firaolmekbib511@gmail.com>
2023-02-20 20:48:03 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints "_putchar" followed by new line.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+fun & nested loop
