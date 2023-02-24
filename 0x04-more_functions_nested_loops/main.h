c9938366ca28e681c72a126e436fcb7faca8cf40
firacil <firaolmekbib511@gmail.com>
2023-02-24 05:56:33 -0800

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@ int _putchar(char c);
 int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
+void print_numbers(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
