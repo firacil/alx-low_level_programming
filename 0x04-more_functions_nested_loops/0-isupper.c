dfc8e98b6b1dec318dbc4172355431e905552c13
firacil <firaolmekbib511@gmail.com>
2023-02-24 04:32:25 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase alphabets.
+ * @c: test number.
+ * Return: 0 or 1.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
