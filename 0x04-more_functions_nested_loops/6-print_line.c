398e072e4be223d39f3730243414417abf736244
firacil <firaolmekbib511@gmail.com>
2023-02-24 06:54:13 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints line on the terminal.
+ * Return: 1 or 0.
+ */
+
+void print_line(int n)
+{
+	int c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for ( c = 0; c < n; c++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
