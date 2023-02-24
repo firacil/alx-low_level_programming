035c645756e1b61bae612b43d5b5ac2742b38329
firacil <firaolmekbib511@gmail.com>
2023-02-24 06:04:24 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9.
+ * return: none.
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
+
