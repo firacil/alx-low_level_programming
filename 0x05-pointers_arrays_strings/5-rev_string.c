d5617f3c1d7031f4fc96fb7d242fab8cfb343e8c
firacil <firaolmekbib511@gmail.com>
2023-02-28 13:59:18 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - prints reverse.
+ * @s: string tested.
+ * Return: none.
+ */
+
+void rev_string(char *s);
+{
+	int l = 0, indx = 0;
+	char tmpo;
+
+	while (s[indx++])
+		l++;
+	for (indx = l - 1; indx >= l / 2; indx--)
+	{
+		tmpo = s[indx];
+		s[indx] = s[len - indx - 1];
+		s[len - indx - 1] = tmp;
+	}
+}
