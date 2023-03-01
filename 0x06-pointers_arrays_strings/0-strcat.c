602d8148767463f9c96ab3a1fd038212094c3fff
firacil <firaolmekbib511@gmail.com>
2023-03-01 10:33:03 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,14 @@
-#include
+#include "main.h"
+
+/**
+ * *_strcat - concatenate two strings.
+ * @dest: the first string.
+ * @src: the 2nd string.
+ * Return: charachter.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat = (dest, src);
+	return (strcat);
+}
