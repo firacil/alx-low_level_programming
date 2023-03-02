8ba8016e5061966878195265a0a7f50f52bcd17d
firacil <firaolmekbib511@gmail.com>
2023-03-02 11:56:25 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (index = 0; src[index] && index < n; index++)
 	dest[index] = src[index];
-	
+
 	for (length = 0; index < n; index++)
 	dest[index] = '\0';
 
