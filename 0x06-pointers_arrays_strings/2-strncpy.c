a62724afad42d2f8d12914a9bb799ff17a3cf84c
firacil <firaolmekbib511@gmail.com>
2023-03-02 11:33:53 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: string to be copied to.
+ * @src: string to be copy.
+ * @n: byte given.
+ * Return: character.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, length = 0;
+
+	while (src[index++])
+		length++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (length = 0; index < n; index++)
+		dest[src] = '\0';
+		return (dest);
+}
