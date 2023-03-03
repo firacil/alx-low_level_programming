202431119288c6dc71506a9246c62f5b642374aa
firacil <firaolmekbib511@gmail.com>
2023-03-02 18:37:30 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters to uppercase.
+ * @str: The string to be changed.
+ * Return: pointer.
+ */
+
+char *string_toupper(char *str)
+
+{
+
+	int index = 0;
+
+	while (str[index])
+	{
+	if (str[index] >= 'a' && str[index] <= 'z')
+	str[index] -= 32;
+	index++;
+	}
+	return (str);
+
+}
