953d55a8353331c0477f2f96bab30a88c5d65b14
firacil <firaolmekbib511@gmail.com>
2023-03-02 18:34:09 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,13 +11,13 @@ void reverse_array(int *a, int n)
 
 {
 
-	init tmpo, index;
+	int tmpo, index;
 
 	for (index = n - 1; index >= n / 2; index--)
 	{
 	tmpo = a[n - 1 - index];
 	a[n - 1 - index] = a[index];
-	a[index] = tmp;
+	a[index] = tmpo;
 	}
 
 }
