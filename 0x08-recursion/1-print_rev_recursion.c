803795089c65cad7aa8397dd3b5c363c3829ee42
firacil <firaolmekbib511@gmail.com>
2023-03-08 06:50:20 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string in reverse.
+ * @s: string.
+ * Return: None.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_print_rev_recursion(s)
+		s--;
+	_putchar(*s);
+}
