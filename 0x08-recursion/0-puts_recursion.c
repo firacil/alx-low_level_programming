588c595a5abd0be22cf90378dc7ad050c10c1367
firacil <firaolmekbib511@gmail.com>
2023-03-08 05:23:16 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string.
+ * @s: string.
+ * Return: None.
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if (s == Null)
+	{
+		return (Null);
+	}
+	return (s);
+}
