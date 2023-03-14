fedb9aaadeddb2f820dd304a42dde5034a4968ae
firacil <firaolmekbib511@gmail.com>
2023-03-14 06:24:49 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of charachter.
+ * @c: charachter.
+ * Return: pointer or Null.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	
+	c [0] = 'H';
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (c == NULL)
+	{
+		return(NULL);
+	}
+}
