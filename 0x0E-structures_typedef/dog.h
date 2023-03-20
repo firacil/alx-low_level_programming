06f90fc6352ea2fb7ccaf916880f973a0731d3c7
firacil <firaolmekbib511@gmail.com>
2023-03-20 13:46:31 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,11 @@
 #define _HEADER_
 
 /**
- * struct dog - holds three combination of structure.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: owner of the dog.
- *
- * Descrption: attr for dog.
- */
-
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  */
 struct dog
 {
 	char *name;
@@ -18,14 +15,12 @@ struct dog
 };
 
 /**
- * dog_t - typedef of struct dog.
+ * dog_t - Typedef for dog.
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
