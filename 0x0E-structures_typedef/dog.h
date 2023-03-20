77294cf6c41eac208a03d4921f382c0bc12107c9
firacil <firaolmekbib511@gmail.com>
2023-03-20 07:20:59 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEAD_
+#define _HEAD_
+
+/**
+ * struct dog - holds three combination of structure.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
