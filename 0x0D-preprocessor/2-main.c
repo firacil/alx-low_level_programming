ba33bdd7db10694a77c0aa549cb21adf95e97d2c
firacil <firaolmekbib511@gmail.com>
2023-03-20 04:51:32 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - printsthe name of the file it compiled from.
+ * char - name of the file.
+ * Return: integer.
+ */
+
+int main (void)
+{
+	char c;
+
+	printf("%s\n", c);
+	return (0);
+}
