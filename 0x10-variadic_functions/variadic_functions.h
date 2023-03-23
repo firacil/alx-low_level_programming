2cc34f55bb389a85389f6bacccacd89aebd4b284
firacil <firaolmekbib511@gmail.com>
2023-03-23 15:21:07 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,18 +2,16 @@
 #define _VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+void print_istrings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_all(const char * const format, ...);
-void print_a_char(char *separator, va_list args);
-void print_a_integer(char *separator, va_list args);
-void print_a_float(char *separator, va_list args);
-void print_a_char_ptr(char *separator, va_list args);
+void print_char(char *separator, va_list ap);
+void print_integ(char *separator, va_list ap);
+void print_float(char *separator, va_list ap);
+void print_char_pt(char *separator, va_list ap);
 
 /**
  * struct format_types - Struct format_types
@@ -23,6 +21,6 @@ void print_a_char_ptr(char *separator, va_list args);
 typedef struct format_types
 {
 	char *identifier;
-	void (*f)(char *separator, va_list args);
+	void (*f)(char *separator, va_list ap);
 } f_dt;
 #endif
