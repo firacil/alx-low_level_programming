dce638fc2b0539c66c88b911bc7c39e2bcd81717
firacil <firaolmekbib511@gmail.com>
2023-03-23 15:14:00 -0700

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,97 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_all - print anything.
+ * @format: specify conversion.
+ * Return: None.
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	f_dt form_types[] = {
+		{ "c", print_a_char },
+		{ "i", print_a_integer },
+		{ "f", print_a_float },
+		{ "s", print_a_char_ptr }
+	};
+
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *separator = "";
+
+	va_start(ap, format);
+
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *form_types[j].identifier)
+			{
+				form_types[j].f(separator, ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+
+	}
+
+	va_end(ap);
+	printf("\n");
+}
+
+/**
+  * print_char - Print character type
+  * @separator: separator of the character
+  * @ap: list of var arguments
+  * Return: None
+  */
+void print_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+  * print_integ - Prints a character of integer type
+  * @separator: separator of the character
+  * @ap: list of variadic arguments
+  * Return: None
+  */
+void print_integ(char *separator, va_list ap)
+{
+	printf("%s%i", separator, va_arg(ap, int));
+}
+
+/**
+  * print_float - Prints a character of float type
+  * @separator: separator of the character
+  * @ap: list of variadic arguments
+  * Return: None
+  */
+void print_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, double));
+}
+
+/**
+  * print_char_pt - Prints the content pointer to char type
+  * @separator: separator of the character
+  * @ap: list of variadic arguments
+  * Return: None
+  */
+void print_char_pt(char *separator, va_list ap)
+{
+	char *arg = va_arg(ap, char *);
+
+	if (arg == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+		return;
+	}
+
+	printf("%s%s", separator, arg);
+}
