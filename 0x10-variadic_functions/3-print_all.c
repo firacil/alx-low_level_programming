d65d7f31bfcf7ae8eeb2c484ee15838d43c452a0
firacil <firaolmekbib511@gmail.com>
2023-03-23 15:31:32 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,10 +12,10 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	f_dt form_types[] = {
-		{ "c", print_a_char },
-		{ "i", print_a_integer },
-		{ "f", print_a_float },
-		{ "s", print_a_char_ptr }
+		{ "c", print_char },
+		{ "i", print_integ },
+		{ "f", print_float },
+		{ "s", print_char_pt }
 	};
 
 	unsigned int i = 0;
