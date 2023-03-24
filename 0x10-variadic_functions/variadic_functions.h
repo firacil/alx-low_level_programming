d894a6558ed94a728549f2db65144d1d48ecff00
firacil <firaolmekbib511@gmail.com>
2023-03-23 23:45:58 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,7 +6,7 @@
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_istrings(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_char(char *separator, va_list ap);
 void print_integ(char *separator, va_list ap);
