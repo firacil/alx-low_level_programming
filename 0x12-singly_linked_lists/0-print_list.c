715e45c2b455619c8a14e8b7ccc88b04d84058a4
firacil <firaolmekbib511@gmail.com>
2023-03-31 15:44:51 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,14 +8,14 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t num = 0;
+	size_t no = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->len);
-		num++;
+		no++;
 		h = h->next;
 	}
 
-	return (num);
+	return (no);
 }
