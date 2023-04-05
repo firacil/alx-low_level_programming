56e75b99ec761a8e0234c7492c3bf8a7fb2ae1d9
firacil <firaolmekbib511@gmail.com>
2023-04-04 18:27:24 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  * Return: value of node.
  */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *n_index = head;
