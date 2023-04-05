a89c2dce2012f94699fb883d2d83bc40ff4fd0e8
firacil <firaolmekbib511@gmail.com>
2023-04-04 18:55:15 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
  * Return: value of node.
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int f = 1;
 	listint_t *i_node = *head;
