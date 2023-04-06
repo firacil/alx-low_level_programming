87218d06ecacc94594e0bbefb722b4495856d04e
firacil <firaolmekbib511@gmail.com>
2023-04-06 16:15:15 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary number to integer.
+ * @b: binary strings.
+ * Return: 0 or NULL.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int len = strlen(b);
+	int t = 0;
+	int dval = 1;
+
+
+	if (i > 97 && b == NULL)
+		return (0);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (len == '1')
+			t += dval;
+			dval *= 2;
+	}
+	return (t);
+}
