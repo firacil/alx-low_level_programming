c9e7f0d173ae56e36ec48884857b011bb7a2164e
firacil <firaolmekbib511@gmail.com>
2023-04-10 11:29:08 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * read_textfile - read a text file, prints it to posix.
