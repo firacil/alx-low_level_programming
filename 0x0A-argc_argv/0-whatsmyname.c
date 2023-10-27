6d34b5c0ff43487dfdbf89c7ace774e12ed67750
firacil <firaolmekbib511@gmail.com>
2023-10-27 15:59:13 -0700

    argv & argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints excutable file name.
+ * @argc: argument count.
+ * @argv: argument vector.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (1);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is Project of Argument count & Argument Vector.
