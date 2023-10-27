00b3db3ca6de62b5257054777b3f571945312d38
firacil <firaolmekbib511@gmail.com>
2023-10-27 08:10:40 -0700

    library updated

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - strings
+ * @s: pointers.
+ * Return: Integer.
+ */
+
+int _atoi(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1);
+}
