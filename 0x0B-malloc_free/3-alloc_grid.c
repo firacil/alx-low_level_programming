9aebfb7b7a445c2e67220d1cbadac51704fef40b
firacil <firaolmekbib511@gmail.com>
2023-10-31 13:37:44 -0700

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * alloc_grid - return pointers to 2d array of integer.
+ * @width: width
+ * @height: height.
+ * Return: pointer tp grid.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **gr;
+	int x, y;
+
+	if (width + height < 2 || width < 1 || height < 1)
+		return (NULL);
+
+	gr = malloc(height * sizeof(*gr));
+	if (gr == NULL)
+		return (NULL);
+
+	for (x = 0; x < height; x++)
+	{
+		gr[x] = malloc(width * sizeof(**gr));
+		if (grid[i] == NULL)
+		{
+			for (x--; x >= 0; x--)
+				free(gr[x]);
+			free(gr);
+			return (NULL);
+		}
+		for (y = 0; y < width; j++)
+			gr[x][y] = 0;
+	}
+
+	return (gr);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * free_grid - free previous memory.
+ * @grid:
+ * @height:
+ * Return: None.
+ */
+
+void free_grid(int **grid, int height)
+{
+
