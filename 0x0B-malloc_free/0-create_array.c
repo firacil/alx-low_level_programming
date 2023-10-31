fb7a16267f2a6284a07593109a6078291d9f7cef
firacil <firaolmekbib511@gmail.com>
2023-10-31 12:12:04 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *str;
 
+	str = malloc(size * sizeof(*str));
 	if (size == 0 && str == NULL)
 	{
 		return (NULL);
 	}
-	str = malloc(size * sizeof(*str));
+	if (str == NULL)
+		return (NULL);
 
 	i = 0;
 	while (i < size)
