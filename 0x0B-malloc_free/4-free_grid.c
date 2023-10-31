b014c24ac91e72c17dc93fed2500579fea850d3e
firacil <firaolmekbib511@gmail.com>
2023-10-31 13:40:44 -0700

    malloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,11 +2,16 @@
 
 /**
  * free_grid - free previous memory.
- * @grid:
- * @height:
+ * @grid: double pointer
+ * @height: grid height.
  * Return: None.
  */
 
 void free_grid(int **grid, int height)
 {
+	int x;
 
+	for (x = 0; x < height; x++)
+		free(grid[x]);
+	free(grid);
+}
