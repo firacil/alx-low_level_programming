aeb2b25bfdbc399d235b99d39fa56e5c4823d9d1
firacil <firaolmekbib511@gmail.com>
2023-11-03 14:24:37 -0700

    more malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,30 @@
 #include "main.h"
 
 /**
- *
+ * _calloc - allocates memory for an array of a certain number entity of elements...
+ * @nmemb: no of elements.
+ * @size: byte size of each array element.
+ * Return: Null or pointer to allocated memory.
+ */
+
+void *_clloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+	char *filr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	m = malloc(size * nmemb);
+
+	if (m == NULL)
+		return (NULL);
+
+	filr = m;
+
+	for (i = 0; i < (size * nmemb); i++)
+		filr[i] = '\0';
+
+	return (m);
+}
