8d1316646095c7c91f0114ace6141a7db4837276
firacil <firaolmekbib511@gmail.com>
2023-11-06 14:02:53 -0800

    struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name;
-	(*d).age;
-	(*d).owner;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
