ad89a9a686b4cde80a72b1708379e91fa6f324ec
firacil <firaolmekbib511@gmail.com>
2023-11-06 14:33:45 -0800

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,18 @@
  * @age: intger
  * @owner: char
  */
-typedef struct dog dog_t
+struct dog dog_t
 {
 
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog.
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
