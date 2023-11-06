8f79f8710bdb6ce936dbfb721e9530190b7d1b03
firacil <firaolmekbib511@gmail.com>
2023-11-06 13:43:57 -0800

    struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Project for struct & typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - have many members
+ * @name: charachter
+ * @age: intger
+ * @owner: char
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
