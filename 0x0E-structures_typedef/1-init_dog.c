4477c9d8bf6f3ebb553e6cddc5a2995e151ef79a
firacil <firaolmekbib511@gmail.com>
2023-11-06 13:58:41 -0800

    struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include "stdio.h"
+
+/**
+ * init_dog - intalize structures.
+ * @d: structure variable
+ * @name: char
+ * @age: intger
+ * @owner: char
+ * Return: none
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name;
+	(*d).age;
+	(*d).owner;
+}
