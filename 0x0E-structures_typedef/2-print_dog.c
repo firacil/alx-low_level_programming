b71ade63f61350de360287f5b6f4e4983f0afed5
firacil <firaolmekbib511@gmail.com>
2023-11-06 14:22:33 -0800

    struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints the struct datamembers value
+ * @d: structure variable.
+ * Return: nothing.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
+
+	printf("Name: %s\n Age: %f\n Owner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
