362d82dd1e0fd10bcadba6aa9e0034d79c6f4e74
firacil <firaolmekbib511@gmail.com>
2023-11-06 18:42:14 -0800

    struct & typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,85 @@
 #include "dog.h"
+#include <stdlib.h>
 
-dog_t *new_dog(char *name, float age, char *owner);
+/**
+ * stlen - returns length of string.
+ * @s: string evaluated.
+ * Return: len of string.
+ */
+int stlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * stcp - copy string.
+ * @dest: pointer to buffer.
+ * @src: string to be copied.
+ * Return: the pointer to dest
+ */
+
+char *stcp(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - create a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to new dog or null if fail.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int l1, l2;
+
+	l1 = stlen(name);
+	l2 = stlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (l1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (l2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	stcp(dog->name, name);
+	stcp(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
