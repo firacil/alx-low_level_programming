21d66bf47b4872d6381c131f0da4a06407b37d31
firacil <firaolmekbib511@gmail.com>
2023-11-08 13:21:34 -0800

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (!array || !cmp)
-		return;
+	if (!array && !cmp)
+		return (-1);
+	else if (size <= 0)
+		return (-1);
 
-	for (index = 0; index < size; index++)
+	if (array && cmp)
 	{
-		if (cmp(array[index] != 0)
-				return (index);
-	}
-
-	return (-1);
+		for (index = 0; index < size; index++)
+		{
+			if (cmp(array[index] != 0)
+					return (index);
+		}
 }
