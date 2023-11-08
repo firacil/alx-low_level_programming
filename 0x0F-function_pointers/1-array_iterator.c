214b007cead0cda168adc04fc676d315e3f9ef24
firacil <firaolmekbib511@gmail.com>
2023-11-08 11:00:57 -0800

    uptodate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 /**
- *
+ * array_iterator - excute parameter given from arr.
+ * @array: array of integer.
+ * @size: size of integer.
+ * @action: function pointer to array.
+ * Return: none
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int num;
+
+	if (size < 1)
+		return;
+	(*action)(num);
+}
