902dc2bc5d2e21dc1c23d4f56152e9afed7f740e
firacil <firaolmekbib511@gmail.com>
2023-11-08 19:10:45 -0800

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int b, i;
+	int bytes, i;
 	char *arr;
 
 	if (argc != 2)
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
 	b = atoi(argv[1]);
 
-	if (b < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
@@ -29,9 +29,9 @@ int main(int argc, char *argv[])
 
 	arr = (char *)main;
 
-	for (i = 0; i < b; i++)
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == b - 1)
+		if (i == bytes - 1)
 		{
 			printf("02hhx\n", arr[i]);
 			break;
