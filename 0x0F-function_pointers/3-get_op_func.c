ce3467269bed2815c3dddfc10c7efb08c2a179b5
firacil <firaolmekbib511@gmail.com>
2023-11-08 21:09:19 -0800

    simple

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,4 +19,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
+	i = 0;
+
+	while(ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,46 @@
-#include "main.h"
+#include "3-calc.h"
 
 /**
- *
+ * main - program that perform simple oprations
+ * @argc: number of argument
+ * @aegv: array of arguments.
+ * Return: 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a1, a2, res;
+	char o;
+	int (*fun)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a1 = atoi(argv[1]);
+	a2 = atoi(argv[3]);
+
+	fun = get_op_func(argv[2]);
+
+	if (!fun)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && a2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = fun(a1, a2);
+
+	printf("%d\n", res);
+
+	return (0);
+}
