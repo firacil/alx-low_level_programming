758f839baedc71e9a59fcf666fc987fc52b154d1
firacil <firaolmekbib511@gmail.com>
2023-11-08 18:50:35 -0800

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,41 @@
 #include "main.h"
 
 /**
- *
+ * main - prints own opcodes.
+ * @argc: no of argument.
+ * @argv: array of argument
+ * Return: 0(sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	int b, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	b = atoi(argv[1]);
+
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < b; i++)
+	{
+		if (i == b - 1)
+		{
+			printf("02hhx\n", arr[i]);
+			break;
+		}
+		printf("02hhx", arr[i]);
+	}
+	return (0);
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,52 @@
-#include "main.h"
+#include "3-calc.h"
 
 /**
- *
+ * op_add - return sum of a & b.
+ * @a: first number.
+ * @b: second number.
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - return difference of a & b.
+ * @a: first number.
+ * @b: second number.
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - return product of a & b.
+ * @a: first number.
+ * @b: second number.
+ * Return:product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - return sum of a & b.
+ * @a: first number.
+ * @b: second number.
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - return reminder of a & b.
+ * @a: first number.
+ * @b: second number.
+ * Return: reminder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
