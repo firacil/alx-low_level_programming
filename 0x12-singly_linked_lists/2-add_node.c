a178677dc1bbd5dd22aab15fcd1acb31841458d5
firacil <firaolmekbib511@gmail.com>
2023-11-16 17:03:02 -0800

    add node at the beginning of LL

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - add a new node at beginnig of LL.
+ * @head: head of the node.
+ * @str: string.
+ * Return: address or NULL if fails.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = (list_t *)malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+
+	if (!new)
+		return (NULL);
+
+	new->next = *head;
+	(*head) = new;
+
+	return (*head);
+}
