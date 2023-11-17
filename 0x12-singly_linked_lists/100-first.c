b54ee2d2ead80eba9a7289c6325ec23d4ce0bfa3
firacil <firaolmekbib511@gmail.com>
2023-11-16 22:39:24 -0800

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before - prints specific string before main.
+ * Return: None.
+ */
+
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
