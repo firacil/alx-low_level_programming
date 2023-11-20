68b295ee47de212d109014a47c1e1e1c6a23e659
firacil <firaolmekbib511@gmail.com>
2023-11-20 07:03:57 -0800

    Singly linked List With C

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,13 +20,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new->n = n;
 	new->next = NULL;
-	
+
 	if ((*head) == NULL)
 	{
 		(*head) = new;
 		return (new);
 	}
-	
+
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
