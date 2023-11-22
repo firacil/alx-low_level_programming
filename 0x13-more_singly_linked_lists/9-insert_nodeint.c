1726af93dcd0e072bb4f4d698a5b9d48e4cc342f
firacil <firaolmekbib511@gmail.com>
2023-11-22 11:51:28 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = (*head);
+		*head = new;
+		return (new);
+	}
 
 	while (i < idx - 1 && temp != NULL)
 	{
