67581790cf39b1f29116d076b5b1f78f9f7a0453
firacil <firaolmekbib511@gmail.com>
2024-01-11 13:37:12 -0800

    updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,7 +5,7 @@
  * @h: pointer to first node.
  * @idx: location to be added.
  * @n: value to be added.
- * Returns: address of new node or NULL.
+ * Return: address of new node or NULL.
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
