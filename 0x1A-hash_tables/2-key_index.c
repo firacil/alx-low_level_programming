da797778987fe2c7fd343aa21c7c395fdc0d500a
firacil <firaolmekbib511@gmail.com>
2024-02-02 13:10:58 -0800

    updated

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of the key.
+ * @key: key of hash table
+ * @size: size of the array of hash table
+ *
+ * Return: the index @key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	return (hash_djb2(key) % size);
+}
