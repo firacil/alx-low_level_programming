8cfcad2126637c988158422ec10c07ffa964a8e8
firacil <firaolmekbib511@gmail.com>
2024-02-02 15:12:38 -0800

    updated

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retreves a value associated with a key
+ * @ht: hash table to be retrieved
+ * @key: key of the hash table
+ *
+ * Return: value associated or NULL (if key couldn't be found)
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int i;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	i = key_index((const unsigned char *)key, ht->size);
+	if (i >= ht->size)
+		return (NULL);
+
+	node = ht->array[i];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
