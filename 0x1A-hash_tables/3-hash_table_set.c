8f1bf9aca63c87d824612c1d8a094f90b01d798b
firacil <firaolmekbib511@gmail.com>
2024-02-02 15:03:45 -0800

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,25 +1,5 @@
 #include "hash_tables.h"
 
-int hash(const char *key, int size);
-
-/**
- * hash - function to hash a key and get index fo ht
- * @key: key of hashtable
- * @size: size of hash table
- *
- * Return: index
- */
-int hash(const char *key, int size)
-{
-	int hash = 0, i;
-
-	for (i = 0; key[i] != '\0'; i++)
-	{
-		hash += key[i];
-	}
-	return (hash % size);
-}
-
 /**
  * hash_table_set - adds an element to hash table
  * @ht: hash table to be updated
@@ -31,50 +11,42 @@ int hash(const char *key, int size)
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int index = hash(key, ht->size);
+	hash_node_t *new;
+	char *vcp;
+	unsigned long int index, i;
 
-	/* createing new value pair */
-	hash_node_t *newval = new_kv_pair(key, value);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	vcp = strdup(value);
+	if (vcp == NULL)
+		return (0);
 
-	/* if slot empty, insert new pair*/
-	if (ht->array[index] == NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		ht->array[index] = newval;
-		return (1);
-	}
-	else
-	{
-		/* if slot is not empty, handle collision by chaning*/
-		hash_node_t *cur = ht->array[index];
-
-		while (cur->next != NULL)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			cur = cur->next;
+			free(ht->array[i]->value);
+			ht->array[i]->value = vcp;
+			return (1);
 		}
-		cur->next = newval;
-		return (1);
 	}
 
-	return (0);
-}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(vcp);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = vcp;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
-/**
- * new_kv_pair - function to create new key-value pair
- * @key: key of hash table
- * @value: value of hash table
- *
- * Return: newly created pair
- */
-hash_node_t *new_kv_pair(const char *key, const char *value)
-{
-	hash_node_t *pair = malloc(sizeof(hash_node_t));
-
-	if (pair == NULL)
-		return (NULL);
-
-	pair->key = strdup(key);
-	pair->value = strdup(value);
-	pair->next = NULL;
-
-	return (pair);
+	return (1);
 }
