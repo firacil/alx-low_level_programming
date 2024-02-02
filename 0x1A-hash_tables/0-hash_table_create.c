8f6c6165b19a658bc65982f866454a2a8b9d54b2
firacil <firaolmekbib511@gmail.com>
2024-02-02 12:39:23 -0800

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 
-	hash_table_t *new_hash_table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
 		return (NULL);
 
 	new_hash_table->size = size;
-	new_hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (new_hash_table->array == NULL)
 		return (NULL);
 
