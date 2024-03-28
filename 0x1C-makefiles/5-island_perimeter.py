173d861c5d0adfee1293cf54da4a268400a4731a
firacil <firaolmekbib511@gmail.com>
2024-03-28 12:17:13 -0700

    updated_solution

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,7 @@
 module to return perimeter of island
 """
 
+
 def island_perimeter(grid):
     """
     solved island perimeter
@@ -15,10 +16,10 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 if i == 0 or grid[i - 1][j] == 0:
                     count += 1
-                if j == 0 or grid[i][j -1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     count += 1
                 if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     count += 1
-                if j == len(grid[i]) -1 or grid[i][j + 1] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     count += 1
     return(count)
